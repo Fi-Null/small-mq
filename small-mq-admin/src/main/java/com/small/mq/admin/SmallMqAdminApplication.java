4256a69c7eeb8da4b797aea6365c1cc1ec52affd
向科 <xiangke6@jd.com>
2019-12-05 15:00:45 +0800

    init

---
+++ small-mq-admin/src/main/java/com/small/mq/admin/SmallMqAdminApplication.java
@@ -0,0 +1,13 @@
+package com.small.mq.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallMqAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallMqAdminApplication.class, args);
+    }
+
+}
